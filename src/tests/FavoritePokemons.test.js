772b2f5afcd7cd0a6379baffb487ebd43ace30bd
washington gama <wgamaneto16@gmail.com>
2022-09-19 15:05:52 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('teste do FavoritePokemon', () => {
+  it('mensagem de not found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const not = screen.getByText('No favorite pokemon found');
+    expect(not).toBeInTheDocument();
+  });
+});
